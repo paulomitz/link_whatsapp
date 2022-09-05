0ff2cc11e65bfdba0c8c6c850134bdfafa1f6ecb
Paulo José Mitz <82454884+paulomitz@users.noreply.github.com>
2022-09-05 17:14:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function iniciarConversa() {
   window.open("https://api.whatsapp.com/send?phone=55" + numeroWhatsapp.value, "_blank");
-  numeroWhatsapp.value = "";
+  numeroWhatsapp.value = " ";
   numeroWhatsapp.focus();
 }
 
