2dba59a129ab2a8912de3a7f79ef9ca9a7c3e959
Paulo José Mitz <82454884+paulomitz@users.noreply.github.com>
2022-09-05 17:02:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var numeroWhatsapp = document.querySelector(".telefone");
 
 document.addEventListener("keypress", function(e) {
   if(e.key === 'Enter') {
-
     var btn = document.querySelector(".enviar");
     btn.click();
   }
