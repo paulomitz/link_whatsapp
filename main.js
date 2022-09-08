087c67e0dc19ad2aa6a835fc5a5f046aeaeab1e7
Paulo José Mitz <82454884+paulomitz@users.noreply.github.com>
2022-09-08 09:56:09 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function iniciarConversa() {
+  window.open("https://api.whatsapp.com/send?phone=55" + numeroWhatsapp.value, "_blank");
+  numeroWhatsapp.value = "";
+  numeroWhatsapp.focus();
+}
+
+var numeroWhatsapp = document.querySelector(".telefone");
+
+var criaLink = document.querySelector(".enviar");
+criaLink.onclick = iniciarConversa;
+
+document.addEventListener("keypress", function(e) {
+  if(e.key === 'Enter') {
+
+    var btn = document.querySelector(".enviar");
+    btn.click();
+  }
+});
